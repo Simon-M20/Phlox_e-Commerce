7c2677d07ea55c094a05091e5e5d3fddf5f35438
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-05-14 15:16:33 -0400

    Update ContactSection.js

--- src/Components/Contact_Section/ContactSection.js
+++ src/Components/Contact_Section/ContactSection.js
@@ -1,9 +1,22 @@
 import React from "react";
+import MapsImg from "../../Assets/Maps.jpg";
 
 const ContactSection = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        alert("Sending pruck pruck prack");
+    };
+
+    const stylesImg = {
+        borderRadius: "25px 25px 25px 25px",
+        overflow: "hidden",
+        boxShadow: "0px 10px 50px 0px #d1d5df",
+    };
+
     return (
-        <section className='flex justify-center items-center'>
-            <article className='md:w-2/5 w-full'>
+        <section className='flex justify-center items-center md:px-10 px-2 py-32'>
+            <article className='md:w-2/5 w-full '>
                 <p
                     className='capitalize font-semibold'
                     style={{ color: "#f42c37" }}>
@@ -17,10 +30,24 @@ const ContactSection = () => {
                     ipsam atque, dolores eligendi ipsa libero eaque dicta
                     quisquam error provident!
                 </p>
-                <form>
-                    <input type='text' placeholder='Your E-mail' />
-                    <input type='text' placeholder='Phone number' />
-                    <textarea placeholder='Your Message Here'></textarea>
+                <form
+                    className='flex flex-col justify-center items-center w-full mt-5'
+                    onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        placeholder='Your E-mail'
+                        className='w-full border rounded-2xl px-6 py-3.5 mb-3 focus:outline-none'
+                    />
+                    <input
+                        type='text'
+                        placeholder='Phone number'
+                        className='w-full border rounded-2xl px-6 py-3.5 mb-3 focus:outline-none'
+                    />
+                    <textarea
+                        placeholder='Your Message Here'
+                        rows='6'
+                        className='w-full rounded-2xl px-5 py-4 border focus:outline-none'
+                        style={{ resize: "none" }}></textarea>
                     <div className='flex justify-end items-end w-4/5'>
                         <button className='aboutBtn'>
                             <span className='aboutBtn-overlay'></span>
@@ -29,7 +56,11 @@ const ContactSection = () => {
                     </div>
                 </form>
             </article>
-            <article></article>
+            <article className='md:w-3/5 w-full'>
+                <figure className='flex w-full justify-end items-end'>
+                    <img src={MapsImg} alt='Google Maps' style={stylesImg} />
+                </figure>
+            </article>
         </section>
     );
 };
