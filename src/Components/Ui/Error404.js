5f1baf6cdc2d4a97b74328d7001dd88179ec3f4b
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-05-14 15:16:37 -0400

    Create Error404.js

---
+++ src/Components/Ui/Error404.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Error404 = () => {
+    return (
+        <div>
+            <Link to='/'>Home</Link>
+            <h1>Error!!</h1>
+        </div>
+    );
+};
+
+export default Error404;
