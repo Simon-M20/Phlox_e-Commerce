5ca963e66124a9a7ecdefef051a4964ea197d955
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-05-13 21:00:18 -0400

    Update Shop.js

--- src/Components/Pages/Shop.js
+++ src/Components/Pages/Shop.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BrandsContainer from "../Home/Brands/BrandsContainer";
 import Footer from "../Home/Footer/Footer";
 import NavBar from "../Home/NavBar/NavBar";
 
@@ -15,12 +16,13 @@ const Shop = () => {
                     <span class='material-icons-outlined mx-2'>
                         keyboard_double_arrow_right
                     </span>
-                    <p>Shop</p>
+                    <p>Products</p>
                 </article>
                 <h2 className='md:text-3xl text-2xl text-black font-extrabold text-center mt-3'>
-                    Shop
+                    Products
                 </h2>
             </section>
+            <BrandsContainer />
             <Footer />
         </section>
     );
